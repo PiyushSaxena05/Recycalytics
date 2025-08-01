20212bd1d584664547a3db956674f0f3b45bf9bf
Piyush Saxena <piyushsaxena168@gmail.com>
2025-08-01 18:21:38 +0530

    Update Recycalytics.java

--- Recycalytics.java
+++ Recycalytics.java
@@ -278,7 +278,7 @@ class Information {
 public class Recycalytics extends Information {
     private static final String url = "jdbc:mysql://localhost:3306/recycle";
     private static final String user = "root";
-    private static final String password = "PIYUSH@111WORD016";
+    private static final String password = "Password007";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         try {
